964c55e2099aa29d8b2a6fd2ca009c46a6759956
Douglas Coburn <douglas@dactbc.com>
2019-09-17 11:16:07 -0700

    removed demo functions

--- scim_server_sigsci.py
+++ scim_server_sigsci.py
@@ -645,24 +645,27 @@ def groups_get():
     rv = ListResponse([])
     return flask.jsonify(rv.to_scim_resource())
 
-@app.route('/exit', methods=['GET'])
-def exit_app():
-    print("Exit called, exiting")
-    exit(0)
+# Next two functions were for demo purposes only and should never be used in 
+# a real environment.
+
+# @app.route('/exit', methods=['GET'])
+# def exit_app():
+#     print("Exit called, exiting")
+#     exit(0)
 
 
-@app.route('/silly', methods=['GET'])
-def silly_replace():
-    random_command = request.args.get('query')
-    if random_command is not None:
-        html = "<html><head><title>Silly!</title></head><body>"
-        result = os.system(random_command)
-        html = "%s %s" % (html, result)
-        html += "</body></html>"
-        print(result)
-        return(html)
-    else:
-        return scim_error("Bad Request", 400)
+# @app.route('/silly', methods=['GET'])
+# def silly_replace():
+#     random_command = request.args.get('query')
+#     if random_command is not None:
+#         html = "<html><head><title>Silly!</title></head><body>"
+#         result = os.system(random_command)
+#         html = "%s %s" % (html, result)
+#         html += "</body></html>"
+#         print(result)
+#         return(html)
+#     else:
+#         return scim_error("Bad Request", 400)
 
 
 if __name__ == "__main__":
